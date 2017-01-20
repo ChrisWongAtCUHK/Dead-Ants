2bdde996008e876d96dcd1dd08aed28069459665
chriswongatcuhk <wc38.wong@gmail.com>
2017-01-20 23:10:04 +0800

    lodimob java solution.

--- java/Dinglemouse.java
+++ java/Dinglemouse.java
@@ -1,20 +1,19 @@
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Dinglemouse {
 
 	public static int deadAntCount(final String ants) {
-		if(ants == null)	return 0;
-		String antRemains = ants.replaceAll("ant", "");
-		
-		return Math.max(count(antRemains, "a"), Math.max(count(antRemains, "n"), count(antRemains, "t")));
-	}
-	
-	/**
-	 * Count the occurrence of c in str
-	 * @param str 	the original string
-	 * @param c		the character to be counted
-	 * @return
-	 */
-	public static int count(String str, String c){
-		return str.length() - str.replace(c, "").length();
+		return ants == null ? 0 : (int) ants
+				.replace("ant", "")
+				.replaceAll("[^ant]", "")
+				.chars()
+				.boxed()
+				.collect(
+						Collectors.groupingBy(Function.identity(),
+								Collectors.counting())).values().stream()
+				.mapToLong(Long::longValue).max().orElse(0L);
 	}
 
 }
